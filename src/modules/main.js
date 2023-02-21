caeadf2d63cb1d08e99449a83cd54020845cc2d5
Boris Savenok <horosh.narod@gmail.com>
2023-02-21 14:05:18 +0200

    доробив модалку

--- src/modules/main.js
+++ src/modules/main.js
@@ -1,8 +1,8 @@
 (() => {
   const refs = {
-    openModalBtn: document.querySelector('[data-modal-open]'),
+    openModalBtn: document.querySelector('[data-modal-open-book-room-modal-window]'),
     closeModalBtn: document.querySelector('[data-modal-close]'),
-    modal: document.querySelector('[data-modal]'),
+    modal: document.querySelector('[data-modal-book-room-modal-window]'),
   };
 
   refs.openModalBtn.addEventListener('click', toggleModal);
@@ -10,6 +10,7 @@
 
   function toggleModal() {
     refs.modal.classList.toggle('is-hidden');
+    document.body.classList.toggle("modal-open")
   }
 
   window.onclick = function (e) {
