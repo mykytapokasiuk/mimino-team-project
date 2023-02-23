5355737c8f7fe393d42af7dda9dac5483473f00b
Boris Savenok <horosh.narod@gmail.com>
2023-02-23 17:52:11 +0200

    модалки з командами на кнопки + багфікс

--- src/modules/main.js
+++ src/modules/main.js
@@ -1,21 +1,24 @@
-
-      (() => {
-  const refs = {
-    openModalBtn3: document.querySelector('[data-modal-open-three]'),
-   
-    closeModalBtn3: document.querySelector("[data-modal-close-three]"),
-    
-    modal3: document.querySelector('[book-room-modal-window-price]'),
-    
-  };
- 
-  refs.openModalBtn3.addEventListener("click", toggleModal3);
-  refs.closeModalBtn3.addEventListener("click", toggleModal3);
- 
-  
-    
-  function toggleModal3() {
-    refs.modal3.classList.toggle("is-hidden");
-    document.body.classList.toggle('modal-open');
-  }
-})();
+document.addEventListener('DOMContentLoaded', function () {
+    const modals = ['book-room-modal', 'book-table-modal', 'room-price-modal'];
+    modals.forEach(element => {
+        const openModalSelector = element + "-open";
+        const closeModalSelector = element + "-close";
+        const openModalBtns = document.querySelectorAll("[" + openModalSelector + "]");
+        const closeModalBtns = document.querySelectorAll("[" + closeModalSelector + "]");
+        const modal = document.querySelector("[" + element + "]");
+        if (!modal) logModalError("Can`t find Modal with attribute " + modal);
+        if (openModalBtns.length === 0) logModalError("Can`t find Open modal button with attribute " + openModalSelector);
+        if (closeModalBtns.length === 0) logModalError("Can`t find Close modal button with attribute " + closeModalSelector);
+        if (!modal || openModalBtns.length === 0 || closeModalBtns.length === 0) return;
+        openModalBtns.forEach(openBtn => openBtn.addEventListener("click", toggleModal));
+        closeModalBtns.forEach(closeBtn =>  closeBtn.addEventListener("click", toggleModal));
+        function toggleModal() {
+            document.body.classList.toggle("modal-open")
+            modal.classList.toggle("is-hidden");
+        }
+    });
+}, false);
+function logModalError(text){
+    const styles = "color: #BADA55";
+    console.log("%c" + text, styles);
+}
